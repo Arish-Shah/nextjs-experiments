a73ed6809c8fa0129b8bf98af73f1e160b4b6d6a
Arish-Shah <arish.r.shah@gmail.com>
2023-04-22 01:18:16 +0200

    update to next.js v13.3.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "nextjs-graphql-experiments",
+  description: "Next.js GraphQL Experiments",
+};
+
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
-const RootLayout = ({ children }: RootLayoutProps) => {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <head />
       <body>{children}</body>
     </html>
   );
-};
-
-export default RootLayout;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-const Home = () => {
+export default function Home() {
   return (
     <main>
       <h1>hello world</h1>
     </main>
   );
-};
-
-export default Home;
+}
